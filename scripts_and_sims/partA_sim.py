c11349bf1a1fb58953c2cf572b70d123108d4820
shafiknassar <shafiknassar@campus.technion.ac.il>
2017-12-24 15:44:02 +0200

    stamstam

--- scripts_and_sims/partA_sim.py
+++ scripts_and_sims/partA_sim.py
@@ -7,12 +7,12 @@ time_per_k_turns = 1
 res = ""
 
 game1 = GameRunner(setup_time, time_per_k_turns, k, 'n', "random_player", "simple_player")
+print("Random player starts:")
 for i in range(3):
     res = game1.run()
 
-#print(res)
-
 game2 = GameRunner(setup_time, time_per_k_turns, k, 'n', "simple_player", "random_player")
+print("Simple player starts:")
 for i in range(3):
     game2.run()
 
